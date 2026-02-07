9a06b8e2154c5b8c8e72132dd824a57846c212ab
Jumyr <morenojumyr0@gmail.com>
2026-02-07 09:16:25 +0800

    enhance the gredesigngame and redesigname and fix bugs

--- Tertris_2_palyer/src/Program.cs
+++ Tertris_2_palyer/src/Program.cs
@@ -9,6 +9,7 @@ namespace Tetris_2_Player
     internal class Program
     {
         private static SoundPlayer backgroundMusic;
+
         static void ShowLandingPage()
         {
             Console.Clear();
@@ -23,63 +24,63 @@ namespace Tetris_2_Player
                 backgroundMusic.PlayLooping();
             }
 
-            const int starCount = 30;
+            const int starCount = 100;
             (int x, int y, bool visible)[] stars = new (int, int, bool)[starCount];
             Random rand = new Random();
 
             for (int i = 0; i < starCount; i++)
             {
-                stars[i] = (rand.Next(0, Console.WindowWidth), rand.Next(0, Console.WindowHeight), rand.NextDouble() > 0.5);
+                stars[i] = (
+                    rand.Next(0, Console.WindowWidth),
+                    rand.Next(0, Console.WindowHeight),
+                    rand.NextDouble() > 0.5
+                );
             }
+
             string[] cloudArt = new string[]
-            {
-        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⡏⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-        "⠤⣤⣤⣤⣤⣤⣤⣤⣤⣿⣿⠇⠀⢿⣿⣷⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⡶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠖⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠂⠀⠀⠀⠀",
-        "⠀⠀⠘⢿⣿⣿⣟⠛⠛⠛⠛⠀⠀⠀⠛⠛⠛⠛⠋⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-        "⠀⠀⠀⠀⠈⠛⣿⣿⣦⡀⠀⠀⠀⠀⠀ ⠀┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-        "⠀⠀⠀⠀⠀⠀⠀⢹⣿⡿⠀⠀⠀⠀⠀ ⠀┃  ███████████ ██████████ ███████████ ███████████   █████  █████████  ┃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-        "⠀⠀⠀⠀⠀⠀⠀⣾⣿⠁⢀⣤⣾⣦⡀ ⠀┃ ░█░░░███░░░█░░███░░░░░█░█░░░███░░░█░░███░░░░░███ ░░███  ███░░░░░███ ┃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-        "⠀⠀⠀⠀⠀⠀⣸⣿⢇⣶⣿⠟⠙⠻⣿ ⠀┃ ░   ░███  ░  ░███  █ ░ ░   ░███  ░  ░███    ░███  ░███ ░███    ░░░  ┃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-        "⠀⠀⠀⠀⠀⢠⣿⣿⠿⠋⠁⠀⠀⠀ ⠳⣄┃     ░███     ░██████       ░███     ░██████████   ░███ ░░█████████  ┃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-        "⠀⠀⠀⠀⠀⡿⠋⠁⠀⠀⠀⠀⠀⠀⠀  ┃     ░███     ░███░░█       ░███     ░███░░░░░███  ░███  ░░░░░░░░███ ┃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-        "                 ┃     ░███     ░███ ░   █    ░███     ░███    ░███  ░███  ███    ░███ ┃" ,
-        "                 ┃     █████    ██████████    █████    █████   █████ █████░░█████████  ┃" ,
-        "                 ┃     ░░░░░    ░░░░░░░░░░    ░░░░░    ░░░░░   ░░░░░ ░░░░░  ░░░░░░░░░  ┃         ",
-        "                 ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛                                  ",
-        "                 ━━━━━━━━━━━━━━━━━━━━━━━━━BSIS 2 PRODUCTION━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-            };
-       
+        {
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⡏⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠤⣤⣤⣤⣤⣤⣤⣤⣤⣿⣿⠇⠀⢿⣿⣷⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⡶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠖⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠂⠀⠀⠀⠀",
+    "⠀⠀⠘⢿⣿⣿⣟⠛⠛⠛⠛⠀⠀⠀⠛⠛⠛┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠈⠛⣿⣿⣦⡀⠀⠀⠀⠀⠀ ⠀┃  ███████████ ██████████ ███████████ ███████████   █████  █████████  ┃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⢹⣿⡿⠀⠀⠀⠀⠀⠀⠀┃ ░█░░░███░░░█░░███░░░░░█░█░░░███░░░█░░███░░░░░███ ░░███  ███░░░░░███ ┃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⣾⣿⠁⢀⣤⣾⣦⡀ ⠀┃ ░   ░███  ░  ░███  █ ░ ░   ░███  ░  ░███    ░███  ░███ ░███    ░░░  ┃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⣸⣿⢇⣶⣿⠟⠙⠻⣿ ⠀┃     ░███     ░██████       ░███     ░██████████   ░███ ░░█████████  ┃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⢠⣿⣿⠿⠋⠁⠀⠀⠀ ⠳⣄┃     ░███     ░███░░█       ░███     ░███░░░░░███  ░███  ░░░░░░░░███ ┃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⡿⠋⠁⠀⠀⠀⠀⠀⠀⠀  ┃     ░███     ░███ ░   █    ░███     ░███    ░███  ░███  ███    ░███ ┃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "                 ┃     █████    ██████████    █████    █████   █████ █████░░█████████  ┃ " ,
+    "                 ┃     ░░░░░    ░░░░░░░░░░    ░░░░░    ░░░░░   ░░░░░ ░░░░░  ░░░░░░░░░  ┃ " ,
+    "                 ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛              ",
+    "                  ━━━━━━━━━━━━━━━━━━━━━━━━━BSIS 2 PRODUCTION━━━━━━━━━━━━━━━━━━━━━━━━━━━━                            "
+ 
+        };
+
             int cloudStartX = 10;
             int cloudStartY = 1;
-
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            for (int i = 0; i < cloudArt.Length; i++)
-            {
-                Console.SetCursorPosition(cloudStartX, cloudStartY + i);
-                Console.Write(cloudArt[i]);
-            }
-
-            int cloudWidth = 0;
-            foreach (string line in cloudArt)
-            {
-                if (line.Length > cloudWidth)
-                    cloudWidth = line.Length;
-            }
-
-            Console.ResetColor();
+            int protectedTop = cloudStartY;
+            int protectedBottom = cloudStartY + cloudArt.Length;
 
             string[] menuItems = { "START GAME", "HIGH SCORE", "EXIT" };
             int selectedIndex = 0;
+            int previousIndex = -1;
             int menuStartY = 19;
+
+            DrawStaticUI(cloudArt, cloudStartX, cloudStartY);
+            DrawMenu(menuItems, selectedIndex, menuStartY);
 
             while (true)
             {
-                Console.Clear();
                 for (int i = 0; i < starCount; i++)
                 {
+                    if (stars[i].y >= protectedTop && stars[i].y <= protectedBottom)
+                        continue;
+
+                    Console.SetCursorPosition(stars[i].x, stars[i].y);
+                    Console.Write(" ");
+
                     if (rand.NextDouble() < 0.3)
                         stars[i].visible = !stars[i].visible;
 
@@ -87,57 +88,16 @@ namespace Tetris_2_Player
                     Console.ForegroundColor = ConsoleColor.White;
                     Console.Write(stars[i].visible ? "⭑" : " ");
                 }
-
                 Console.ResetColor();
 
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                for (int i = 0; i < cloudArt.Length; i++)
+
+                if (previousIndex != selectedIndex)
                 {
-                    Console.SetCursorPosition(cloudStartX, cloudStartY + i);
-                    Console.Write(cloudArt[i]);
-                }
-               
-
-
-                for (int i = 0; i < menuItems.Length; i++)
-                {
-                    int menuWidth = menuItems[i].Length + 4;
-                    int menuHeight = 3;
-                    int startX = (Console.WindowWidth - menuWidth) / 2;
-                    int startYMenu = menuStartY + i * 3;
-
-                    for (int y = 0; y < menuHeight; y++)
-                    {
-                        Console.SetCursorPosition(startX, startYMenu + y);
-                        Console.Write(new string(' ', menuWidth));
-                    }
+                    DrawMenu(menuItems, selectedIndex, menuStartY);
+                    previousIndex = selectedIndex;
                 }
 
-                for (int i = 0; i < menuItems.Length; i++)
-                {
-                    int menuWidth = menuItems[i].Length + 4;
-                    int startX = (Console.WindowWidth - menuWidth) / 2;
-                    int startYMenu = menuStartY + i * 3;
-
-                    if (i == selectedIndex)
-                    {
-                        Console.SetCursorPosition(startX, startYMenu);
-                        Console.Write("┏" + new string('━', menuWidth - 2) + "┓");
-
-                        Console.SetCursorPosition(startX, startYMenu + 1);
-                        Console.Write("┃ " + menuItems[i].PadRight(menuWidth - 4) + " ┃");
-
-                        Console.SetCursorPosition(startX, startYMenu + 2);
-                        Console.Write("┗" + new string('━', menuWidth - 2) + "┛");
-                    }
-                    else
-                    {
-                        Console.SetCursorPosition(startX + 2, startYMenu + 1);
-                        Console.Write(menuItems[i]);
-                    }
-                }
-
-                System.Threading.Thread.Sleep(150);
+                System.Threading.Thread.Sleep(120);
 
                 if (Console.KeyAvailable)
                 {
@@ -145,27 +105,79 @@ namespace Tetris_2_Player
                     switch (key)
                     {
                         case ConsoleKey.UpArrow:
-                            selectedIndex--;
-                            if (selectedIndex < 0) selectedIndex = menuItems.Length - 1;
+                            selectedIndex = (selectedIndex - 1 + menuItems.Length) % menuItems.Length;
                             break;
+
                         case ConsoleKey.DownArrow:
-                            selectedIndex++;
-                            if (selectedIndex >= menuItems.Length) selectedIndex = 0;
+                            selectedIndex = (selectedIndex + 1) % menuItems.Length;
                             break;
+
                         case ConsoleKey.Enter:
-                            switch (selectedIndex)
+                            if (selectedIndex == 0)
+                                return;
+
+                            if (selectedIndex == 1)
                             {
-                                case 0: return;
-                                case 1: ShowHighScore(); break;
-                                case 2: Environment.Exit(0); break;
+                                ShowHighScore();
+
+                                Console.Clear();
+                                DrawStaticUI(cloudArt, cloudStartX, cloudStartY);
+                                DrawMenu(menuItems, selectedIndex, menuStartY);
+                                previousIndex = -1; 
                             }
+
+                            if (selectedIndex == 2)
+                                Environment.Exit(0);
+
                             break;
+
                     }
                 }
             }
-
         }
 
+        static void DrawStaticUI(string[] cloudArt, int x, int y)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            for (int i = 0; i < cloudArt.Length; i++)
+            {
+                Console.SetCursorPosition(x, y + i);
+                Console.Write(cloudArt[i]);
+            }
+            Console.ResetColor();
+        }
+
+        static void DrawMenu(string[] menuItems, int selectedIndex, int menuStartY)
+        {
+            for (int i = 0; i < menuItems.Length; i++)
+            {
+                int width = menuItems[i].Length + 4;
+                int startX = (Console.WindowWidth - width) / 2;
+                int y = menuStartY + i * 3;
+
+                Console.SetCursorPosition(startX, y);
+                Console.Write(new string(' ', width));
+                Console.SetCursorPosition(startX, y + 1);
+                Console.Write(new string(' ', width));
+                Console.SetCursorPosition(startX, y + 2);
+                Console.Write(new string(' ', width));
+
+                if (i == selectedIndex)
+                {
+                    Console.SetCursorPosition(startX, y);
+                    Console.Write("┏" + new string('━', width - 2) + "┓");
+                    Console.SetCursorPosition(startX, y + 1);
+                    Console.Write("┃ " + menuItems[i] + " ┃");
+                    Console.SetCursorPosition(startX, y + 2);
+                    Console.Write("┗" + new string('━', width - 2) + "┛");
+                }
+                else
+                {
+                    Console.SetCursorPosition(startX + 2, y + 1);
+                    Console.Write(menuItems[i]);
+                }
+            }
+        }
 
         static void ShowHighScore()
         {
@@ -181,51 +193,35 @@ namespace Tetris_2_Player
             Console.WriteLine("==== HIGH SCORES ====\n");
 
             if (scores.Count == 0)
-            {
                 Console.WriteLine("No high scores found.");
-            }
             else
-            {
                 for (int i = 0; i < Math.Min(5, scores.Count); i++)
-                {
                     Console.WriteLine($"{i + 1}. {scores[i]}");
-                }
-            }
 
             Console.WriteLine("\nPress any key to return...");
             Console.ReadKey(true);
+
+            Console.Clear();
+            return;
         }
+
         static void Main(string[] args)
-            {
+        {
             Console.OutputEncoding = Encoding.Unicode;
             Console.CursorVisible = false;
-
-            Console.Write("\u001b[?25l"); 
-            Console.Write("\u001b[2J");  
-
             Console.Title = "2-Player Tetris Game";
 
-                try
-                {
-                    int maxWidth = Console.LargestWindowWidth;
-                    int maxHeight = Console.LargestWindowHeight;
-
-                    Console.SetWindowSize(Math.Min(maxWidth, 120), Math.Min(maxHeight, 50));
-                    Console.SetBufferSize(Math.Min(maxWidth, 120), Math.Min(maxHeight, 50));
-                }
-                catch
-                {
-                    Console.WindowWidth = 70;
-                    Console.WindowHeight = 45;
-                    Console.BufferWidth = 70;
-                    Console.BufferHeight = 45;
-                }
-
-                ShowLandingPage();
-
-                Game game = new Game();
-                game.Run();
+            try
+            {
+                Console.SetWindowSize(120, 45);
+                Console.SetBufferSize(120, 45);
             }
+            catch { }
+
+            ShowLandingPage();
+
+            Game game = new Game();
+            game.Run();
         }
     }
-
+}
